fea7c9c7364f7425f4aae4ae93ce6763d0192786
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2023-09-08 19:14:10 -0600

    added prompts for length as well as the optPool

--- assets/script.js
+++ assets/script.js
@@ -4,18 +4,29 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword(useUpperCase, useLowerCase, useNumbers, useSymbols) {
   console.log("Clicked!");
 
-  let optPool = ""
+  var optPool = ""
 
   const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   const lower = "abcdefghijklmnopqrstyvwxyz"
   const nums = "0123456789"
   const symb = "!@#$%^&*()-_=+"
-  
+
+  const length = parseInt(promt("Enter password lenght, MUST be between 8 and 128.")); 
+
+  if (isNaN(length) || length < 8 || length > 128){
+    alert("You must choose a number between 8 and 128 please.");
+    return null;
+  }
 
   if (useUpperCase) optPool += upper;
   if (useLowerCase) optPool += lower;
   if (useNumbers) optPool += nums;
   if (useSymbols) optPool += symb;
+
+  const useUpperCase = confirm("Would you like to include Capitol letters? Cancel = No");
+  const useLowerCase = confirm("Would you like to include lowercase letters? Cancel = No");
+  const useNumbers = confirm("would you like to include numbers? Cancel = No");
+  const useSymbols = confirm("Would you like to include Symbols? Cancel = No");
 
   if (optPool === "") {
     alert("At least on character option must be chosen!")
