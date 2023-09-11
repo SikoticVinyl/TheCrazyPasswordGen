c2b34efc96a2ff4264c116885e6ea9ad5995bf83
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2023-09-10 22:40:57 -0600

    Re-did confirm messages after research, got them to work and react if true.

--- assets/script.js
+++ assets/script.js
@@ -11,22 +11,45 @@ function generatePassword(useUpperCase, useLowerCase, useNumbers, useSymbols) {
   const nums = "0123456789"
   const symb = "!@#$%^&*()-_=+"
 
-  const length = parseInt(promt("Enter password lenght, MUST be between 8 and 128.")); 
+  const length = parseInt(prompt("Enter password lenght, MUST be between 8 and 128.")); 
 
   if (isNaN(length) || length < 8 || length > 128){
     alert("You must choose a number between 8 and 128 please.");
     return null;
   }
 
-  if (useUpperCase) optPool += upper;
-  if (useLowerCase) optPool += lower;
-  if (useNumbers) optPool += nums;
-  if (useSymbols) optPool += symb;
 
-  const useUpperCase = confirm("Would you like to include Capitol letters? Cancel = No");
-  const useLowerCase = confirm("Would you like to include lowercase letters? Cancel = No");
-  const useNumbers = confirm("would you like to include numbers? Cancel = No");
-  const useSymbols = confirm("Would you like to include Symbols? Cancel = No");
+ if (confirm("Would you like to include Capitol letters?")) {
+  useUpperCase = true;
+ }
+ if (useUpperCase) {
+  optPool += upper;
+ }
+
+
+ if (confirm("Would you like to include lowercase letters?")){
+  useLowerCase = true;
+ }
+ if (useLowerCase) {
+  optPool += lower;
+ }
+
+
+ if (confirm("Would you like to include numbers?")){
+  useNumbers = true;
+ }
+ if (useNumbers) {
+  optPool += nums;
+ }
+
+
+ if (confirm("Would you like to include Symbols?")){
+  useSymbols = true;
+ }
+ if (useSymbols) {
+  optPool += symb;
+ }
+
 
   if (optPool === "") {
     alert("At least on character option must be chosen!")
